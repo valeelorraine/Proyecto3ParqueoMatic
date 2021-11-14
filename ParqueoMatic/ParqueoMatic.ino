24d4f08e7e7c5af57d735c5c657101f5a5f38dcc
JoseLuisA-P <alv19392@uvg.edu.gt>
2021-11-14 11:16:54 -0600

    pagina web y JSON
    pagina web de datos terminada y JSON colocados para guardar los datos

--- ParqueoMatic/ParqueoMatic.ino
+++ ParqueoMatic/ParqueoMatic.ino
@@ -7,6 +7,13 @@
 //Variables para el WebServer
 const char* ssid = "ParqueoMatic";
 const char* pass = "12345678";
+char dato;
+uint8_t posicion = 0;
+
+String recibido,dummy;
+String dato1,dato2,dato3,dato4;
+
+int datos[4];
 
 IPAddress ipLocal(192,168,1,1);
 IPAddress gateway(192,168,1,1);
@@ -14,8 +21,9 @@ IPAddress subnet(255,255,255,0);
 
 WebServer server(80);
 
-uint8_t carros1 = 0;
-uint8_t carros2 = 0;
+StaticJsonDocument<1024> datosJson; //crear un documento para guardar los valores
+JsonArray fechas = datosJson.createNestedArray("fechas");
+JsonArray parqueo = datosJson.createNestedArray("parqueo");
 
 void setup() {
   Serial.begin(115200);
@@ -25,6 +33,7 @@ void setup() {
       return;
     }
     
+  /*
   File archivoJSON = SPIFFS.open("/datos.json","r");
   if(!archivoJSON){
     Serial.println("error leyendo el JSON");
@@ -32,7 +41,7 @@ void setup() {
     }  
 
   size_t datos = archivoJSON.size();
-  if(datos > 200){ //abrirlo y ver que no tenga un tamaño mayor al requerido
+  if(datos > 1024){ //abrirlo y ver que no tenga un tamaño mayor al requerido
     Serial.println("El archivo es demasiado grande");
     return;
     }
@@ -41,20 +50,25 @@ void setup() {
   std::unique_ptr<char[]>buf(new char[datos]);
   archivoJSON.readBytes(buf.get(), datos);
   Serial.println(buf.get()); //ver si se cargo de forma correcta el archivo
+  
+  StaticJsonDocument<1024> datosJson2;
+  deserializeJson(datosJson2,buf.get());
 
-  StaticJsonDocument<200> datosJson; //crear un documento para guardar los valores
-  deserializeJson(datosJson, buf.get());
-  carros1 = datosJson["Parqueo1"]; //se crean las variables individuales que se almacenaran
-  carros2 = datosJson["Parqueo2"];
-
+  for(int i=0;i<5;i++){
+    fechas.add(datosJson2["fechas"][i]);
+    parqueo.add(datosJson2["parqueo"][i]);
+    }
+    
+  serializeJsonPretty(datosJson, Serial);
   archivoJSON.close(); //se cierra luego de abrir el archivo y modificarlo
+  */
   
   Serial.print("Configurando el access point ....."); //configuracion del accesspoint para enlazar la app
   WiFi.softAP(ssid,pass);
   WiFi.softAPConfig(ipLocal,gateway,subnet);
   delay(100);
 
-  server.on("/", paginaInicio2);
+  server.on("/", leerHTML);
   server.begin(); //iniciar el servidor
   Serial.println("se inicio el servidor");
   delay(100);
@@ -63,23 +77,43 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  
+  if(Serial.available()){
+    posicion += 1;
+    recibido = Serial.readStringUntil('\n');
+    if(recibido != "a"){
+    if(fechas.size() > 5){
+        fechas.remove(0);
+        parqueo.remove(0);
+        }
+    fechas.add(posicion);
+    parqueo.add(recibido);
+    leerHTML();
+    /*
+    serializeJsonPretty(datosJson, Serial);
+    const char* dato10 = datosJson["parqueo"][0];
+    dummy = dato10;
+    
+    for(int i=0; i<4;i++){
+      int index = dummy.indexOf(','); //mira el index del dato
+      datos[i]= dummy.substring(0,index).toInt(); //lo separa en una substring hasta ese index
+      dummy = dummy.substring(index + 1); //crea la nueva string en base a la anterior quitando el dato
+      Serial.println(datos[i]); //debugeo
+      }
+      */
+    }
+    else{
+      guardarJson();
+      }  
+    }
+    
 }
 
 void guardarJson(){
   //Usado para modificar el valor del JSON en el SPIFF
-  StaticJsonDocument<100> datosParqueo; //JSON local colocado en ESP32 para las variables que se guardaran
-  datosParqueo["Parqueo1"] = carros1;
-  datosParqueo["Parqueo2"] = carros2;
-
   File archivoGuardar = SPIFFS.open("/datos.json","w"); //se abre el documento
-  serializeJson(datosParqueo,archivoGuardar); //se serializa el JSON del ESP32 al archivo abierto y se modifica este JSON
+  serializeJson(datosJson,archivoGuardar); //se serializa el JSON del ESP32 al archivo abierto y se modifica este JSON
   archivoGuardar.close(); //se cierra el archivo
-  
-  }
-
-void paginaInicio(void){
-  server.send(200,"text/html", enviarHTMLUpdate()); //para enviar la pagina de inicio
+  Serial.println("Se guardo el JSON");
   }
 
 void paginaInicio2(void){ //enviar una pagina de inicio utilizando el SPIFF
@@ -88,16 +122,55 @@ void paginaInicio2(void){ //enviar una pagina de inicio utilizando el SPIFF
   archivo.close();
   }
 
-String enviarHTMLUpdate(void){ //HTML variable que se envia a el cliente
-  String val = "<!doctype html>\n";
-  val += "<html>\n";
-  val += "<head>\n";
-  val += "<title>Prueba </title>\n";
-  val += "</head>\n";
-  val += "<body>\n";
-  val += "<h1> Primera pagina </h1>\n";
-  val += "Primera pagina\n";
-  val += "</body>\n";
-  val += "</html>\n";
-  return val;
+void leerHTML(void){
+  File HTMLdin = SPIFFS.open("/indexvariable.html","r");
+  String datosHTML = HTMLdin.readString();
+  /*datosHTML += "<tr>";
+  datosHTML += "<td>2</td>";
+  datosHTML += "<td>Abril 2020</td>";
+  datosHTML += "<td>2</td>";
+  datosHTML += "<td>2</td>";
+  datosHTML += "<td>2</td>";
+  datosHTML += "<td>2</td>";
+  datosHTML += "</tr>";
+  datosHTML += "</table>";
+  datosHTML += "</html>";*/
+  
+  for(int i=0; i<fechas.size();i++){
+    datosHTML += "<tr align =\"center\" >\n"; //inicio de la fila
+    
+    datosHTML += "<td>"; //celda de numero
+    datosHTML += (i);
+    datosHTML += "</td>\n";
+
+    datosHTML += "<td>"; //celda de fecha
+    const char* dummyfecha = datosJson["fechas"][i];
+    dummy = dummyfecha;
+    datosHTML += dummy;
+    datosHTML += "</td>\n";
+    
+    const char* dato10 = datosJson["parqueo"][i];
+    dummy = dato10;
+    
+    for(int i=0; i<4;i++){
+      int index = dummy.indexOf(','); //mira el index del dato
+      datos[i]= dummy.substring(0,index).toInt(); //lo separa en una substring hasta ese index
+      dummy = dummy.substring(index + 1); //crea la nueva string en base a la anterior quitando el dato
+      }
+      
+    for(int i=0; i<4;i++){
+      datosHTML += "<td>"; //celda de numero
+      datosHTML += datos[i];
+      datosHTML += "</td>\n";
+      }
+    
+    
+    datosHTML += "</tr>\n"; //din de la fila para la siguiente
+    }
+
+  datosHTML += "</table>";
+  datosHTML += "</html>";
+  //Serial.println(datosHTML);
+  server.send(200,"text/html",datosHTML);
+  HTMLdin.close();
   }
